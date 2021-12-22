d40caef4950b4b6c1bce19ad5efb218dcd734337
Puvendhan <puvendhan14@gmail.com>
2021-12-22 20:11:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ resource "aws_instance" "LabInstance" {
  ami         = "ami-052cef05d01020f1d"
  instance_type = "t2.micro"
  key_name      = "aws"
- count         = "2"
+ count         = "1"
 }
 
 resource "aws_instance" "Test_Instance" {
  ami         = "ami-052cef05d01020f1d"
  instance_type = "t2.micro"
  key_name      = "aws"
- count         = "1"
+ count         = "2"
 }
