72b4f4d03854199b2f56e45f396e86cf94baf812
Puvendhan <puvendhan@Puvendhans-MacBook-Pro.local>
2023-03-02 08:30:15 +0530

    updated the scripts

--- appinstance.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "App_Instance" {
- ami         = "ami-052cef05d01020f1d"
- instance_type = "t2.micro"
- key_name      = "aws"
- count         = "1"
-}
--- main.tf
+++ main.tf
@@ -1,19 +1,21 @@
-provider "aws" {
- access_key = "AKIASHNIPAMUQ767F5QV"
- secret_key = "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"
- region     = "ap-south-1"
+resource "aws_instance" "web-instance" {
+    ami = var.ami
+    key_name = var.key_name
+    count = var.instancecount
+    instance_type = var.instancetype
+    tags = {
+        Name = "Webapp-linux"
+        Owner = "devteam"
+    }    
 }
 
-resource "aws_instance" "LabInstance" {
- ami         = "ami-052cef05d01020f1d"
- instance_type = "t2.micro"
- key_name      = "aws"
- count         = "1"
-}
-
-resource "aws_instance" "Test_Instance" {
- ami         = "ami-052cef05d01020f1d"
- instance_type = "t2.micro"
- key_name      = "aws"
- count         = "2"
+resource "aws_instance" "new-instance" {
+    ami = var.ami
+    key_name = var.key_name
+    count = 2
+    instance_type = var.instancetype
+    tags = {
+        Name = "newapp-linux"
+        Owner = "devteam"
+    }
 }
